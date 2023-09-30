fbde87c708badb72768b8d17c2540cef288362aa
habaccc <haba.cicici@gmail.com>
2023-10-01 02:35:01 +0900

    MySQL JOIN 글생성

--- src/main.js
+++ src/main.js
@@ -173,23 +173,41 @@ var app = http.createServer(function(request,response){
       }
     } else if(pathname === '/create'){
       db.query(`SELECT * FROM topic`, function(error, topics){
-        var title = 'Create';
-        var list = template.list(topics);
-        var html = template.HTML(title, list,
-        ` <form action="/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
+        db.query(`SELECT * FROM author`, function(error2, authors){
+          console.log(authors);
+
+          // template.js에 옮겨서 사용
+          /*
+          var tag = '';
+          var i = 0;
+          while(i < authors.length){
+            tag += `<option value="${authors[i].id}">${authors[i].name}</option>`;
+            i++;
+          }
+          */
+
+          var title = 'Create';
+          var list = template.list(topics);
+          var html = template.HTML(title, list,
+          ` 
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              ${template.authorSelect(authors)}
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,
+          `<a href="/create">create</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
       });
   } else if(pathname === '/create_process'){
     var body ='';
@@ -209,7 +227,7 @@ var app = http.createServer(function(request,response){
       db.query(`
         INSERT INTO topic (title, description, created, author_id) 
         VALUES(?, ?, NOW(), ?)`,
-        [post.title, post.description, 1],
+        [post.title, post.description, post.author],
         function(error, result){
           if(error){
             throw error;
