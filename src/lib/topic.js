a39a490495954c3f813fdf2ebeba9733d5f4e11c
habaccc <haba.cicici@gmail.com>
2023-10-04 00:43:19 +0900

    escaping - sanitizeHtml

--- src/lib/topic.js
+++ src/lib/topic.js
@@ -3,6 +3,7 @@ var template = require('./template.js');
 var db = require('./db');
 var url = require('url');
 var qs = require('querystring');
+var sanitizeHtml = require('sanitize-html'); // 사용자가 입력한 정보를 살균(?)해줌. 입력한 정보가 html에서 문제가 생길 수 있기 때문에
 
 exports.home = function(request, response){
     db.query(`SELECT * FROM topic`, function(error, topics){
@@ -23,7 +24,7 @@ exports.page = function(request, response){
     var queryData = url.parse(_url, true).query;
     db.query(`SELECT * FROM topic`, function(error, topics){
         if(error){
-          throw error2;
+          throw error;
         }
         db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`, [queryData.id], function(error2, topic){
           if(error2){
@@ -34,9 +35,9 @@ exports.page = function(request, response){
           var description = topic[0].description;
           var list = template.list(topics);
           var html = template.HTML(title, list,
-            `<h2>${title}</h2>
-            ${description}
-            <p> by ${topic[0].name}</p>
+            `<h2>${sanitizeHtml(title)}</h2>
+            ${sanitizeHtml(description)}
+            <p> by ${sanitizeHtml(topic[0].name)}</p>
             `,
             ` <a href="/create">create</a>
             <a href="/update?id=${queryData.id}">update</a>
@@ -67,7 +68,7 @@ db.query(`SELECT * FROM topic`, function(error, topics){
 
       var title = 'Create';
       var list = template.list(topics);
-      var html = template.HTML(title, list,
+      var html = template.HTML(sanitizeHtml(title), list,
       ` 
       <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
@@ -132,13 +133,13 @@ db.query(`SELECT * FROM topic`, function(error, topics){
       }
         db.query(`SELECT * FROM author`, function(error2, authors){
           var list = template.list(topics);
-          var html = template.HTML(topic[0].title, list,
+          var html = template.HTML(sanitizeHtml(topic[0].title), list,
             `
             <form action="/update_process" method="post">
               <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+              <p><input type="text" name="title" placeholder="title" value="${sanitizeHtml(topic[0].title)}"></p>
               <p>
-                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                <textarea name="description" placeholder="description">${sanitizeHtml(topic[0].description)}</textarea>
               </p>
               <p>
                 ${template.authorSelect(authors, topic[0].author_id)}
